107d266253fe8f76a873261ba3334b3b87d7a94f
Raphael Störk <raphael.stoerk@uni-ulm.de>
2019-01-13 20:45:36 +0100

    Added Comments
    Added comments to make the Code more self-explanatory, also added a bit of text that can be displayed in the info screen

--- app/src/main/java/de/uni_ulm/ismm/stolperpfad/MainMenuActivity.java
+++ app/src/main/java/de/uni_ulm/ismm/stolperpfad/MainMenuActivity.java
@@ -7,28 +7,47 @@ import android.view.View;
 
 import com.androidquery.AQuery;
 
+/**
+ * This class the current entry point to our activities, this might change with an added
+ * splash screen. For now, this is the first activity a user will be able to interact with
+ */
 public class MainMenuActivity extends AppCompatActivity {
 
+    // The AQuery framework lets us write short understandable code, see further down
     private AQuery aq;
+
+    // Buttons and similar Components need Listeners to do stuff when they are pressed
     private MyClickListener myListener;
 
+    /**
+     * This is what happens when this activity is first started
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Initialize this view and display the right screen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-
+        // Initialize important helper-Objects
         aq = new AQuery(this);
         myListener = new MyClickListener();
 
+        // add the listener to the buttons on screen and make them visible
         aq.id(R.id.exit_button).visible().clicked(myListener);
         aq.id(R.id.info_button).visible().clicked(myListener);
     }
 
+    /**
+     * This is an internal class that handles the Clicks of buttons on the main menu
+     */
     class MyClickListener implements View.OnClickListener {
+
         @Override
         public void onClick(View v) {
             Intent intent;
+
+            // a simple switch case statement that checks which button was pressed
             switch (v.getId()) {
                 case R.id.exit_button:
                     finish();
--- app/src/main/java/de/uni_ulm/ismm/stolperpfad/ScrollingInfoActivity.java
+++ app/src/main/java/de/uni_ulm/ismm/stolperpfad/ScrollingInfoActivity.java
@@ -2,12 +2,37 @@ package de.uni_ulm.ismm.stolperpfad;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import com.androidquery.AQuery;
+
+/**
+ * This is a simple example activity that displays scrollable text,
+ * this class is only important to actually get the app to show the right screen,
+ * the content and interface things are in the corresponding xml layout file
+ */
 public class ScrollingInfoActivity extends AppCompatActivity {
 
+    /**
+     * This is what happens when this activity is first started
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Initialize this view and display the right screen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling_info);
+
+        AQuery aq = new AQuery(this);
+
+        // Lets the button do something, this time with a anonymous class
+        aq.id(R.id.floatingActionButton).visible().clicked(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // makes the current screen go away
+                finish();
+            }
+        });
     }
+
 }

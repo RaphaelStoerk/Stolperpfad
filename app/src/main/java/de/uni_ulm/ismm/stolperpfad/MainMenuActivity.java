7ff8f9e1d551891fdc811864cd949493af8c73ba
Raphael Störk <raphael.stoerk@uni-ulm.de>
2019-01-13 19:14:06 +0100

    AQuery and basic functions
    Added the AQuery Framework and added two simple buttons to the main menu, also added a new activity as an example for a info scrolling view

--- app/src/main/java/de/uni_ulm/ismm/stolperpfad/MainMenuActivity.java
+++ app/src/main/java/de/uni_ulm/ismm/stolperpfad/MainMenuActivity.java
@@ -1,13 +1,43 @@
 package de.uni_ulm.ismm.stolperpfad;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.androidquery.AQuery;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    private AQuery aq;
+    private MyClickListener myListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+
+        aq = new AQuery(this);
+        myListener = new MyClickListener();
+
+        aq.id(R.id.exit_button).visible().clicked(myListener);
+        aq.id(R.id.info_button).visible().clicked(myListener);
+    }
+
+    class MyClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Intent intent;
+            switch (v.getId()) {
+                case R.id.exit_button:
+                    finish();
+                    System.exit(0);
+                    break;
+                case R.id.info_button:
+                    intent = new Intent(MainMenuActivity.this, ScrollingInfoActivity.class);
+                    startActivity(intent);
+            }
+        }
     }
 }

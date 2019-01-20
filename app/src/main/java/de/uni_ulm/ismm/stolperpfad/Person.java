d15be44731de15c291ec7c56ec553e1428c2e495
Ulrike <ulrike.kulzer@uni-ulm.de>
2019-01-20 16:37:45 +0100

    database stuff 2

--- app/src/main/java/de/uni_ulm/ismm/stolperpfad/Person.java
+++ app/src/main/java/de/uni_ulm/ismm/stolperpfad/Person.java
@@ -1,4 +1,14 @@
 package de.uni_ulm.ismm.stolperpfad;
 
+import android.support.annotation.NonNull;
+
+/*
+This class is the entity class of a person.
+ */
 public class Person {
+    private String pName;
+
+    public Person(@NonNull String name){this.pName = name;}
+
+    public String getName(){return this.pName;}
 }

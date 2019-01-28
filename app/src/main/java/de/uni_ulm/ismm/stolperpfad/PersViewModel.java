dc370ec0cf2b4a89a1fda5612d3dab006453a53c
Ulrike <ulrike.kulzer@uni-ulm.de>
2019-01-28 19:00:30 +0100

    safe push

---
+++ app/src/main/java/de/uni_ulm/ismm/stolperpfad/PersViewModel.java
@@ -0,0 +1,27 @@
+package de.uni_ulm.ismm.stolperpfad;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+
+import java.util.List;
+
+public class PersViewModel extends AndroidViewModel {
+
+    private PersRepository mRepo;
+    private List<Person> mAllPersons;
+
+    public PersViewModel (Application application){
+        super(application);
+        mRepo = new PersRepository(application);
+        mAllPersons = mRepo.getAllPersons();
+    }
+
+    List<Person> getAllPersons(){
+        return mAllPersons;
+    }
+
+    public void insert(Person person){
+        mRepo.insert(person);
+    }
+
+}

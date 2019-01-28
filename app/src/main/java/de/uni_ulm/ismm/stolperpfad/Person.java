ed220aa8b09ff9932432f298bd2f00ebe776207a
Ulrike <ulrike.kulzer@uni-ulm.de>
2019-01-28 12:08:07 +0100

    Versuch 2

---
+++ app/src/main/java/de/uni_ulm/ismm/stolperpfad/Person.java
@@ -0,0 +1,29 @@
+package de.uni_ulm.ismm.stolperpfad;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "pers_table")
+public class Person {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @NonNull
+    @ColumnInfo(name = "first_name")
+    private String mFstName;
+
+    @NonNull
+    @ColumnInfo(name = "family_name")
+    private String mFamName;
+
+    public Person (@NonNull String fstName, @NonNull String famName) {
+        this.mFstName = fstName;
+        this.mFamName = famName;
+    }
+
+    public String getFstName(){return this.mFstName;}
+    public String getFamName(){return this.mFamName;}
+}

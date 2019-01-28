171e126c5e4f53b363e0540cfb6c9b803c357b97
Ulrike <ulrike.kulzer@uni-ulm.de>
2019-01-28 12:08:33 +0100

    rest

--- app/src/main/java/de/uni_ulm/ismm/stolperpfad/PerRoomDatabase.java
+++ app/src/main/java/de/uni_ulm/ismm/stolperpfad/PerRoomDatabase.java
@@ -1,4 +1,30 @@
 package de.uni_ulm.ismm.stolperpfad;
 
-public abstract class PerRoomDatabase {
+import android.arch.persistence.db.SupportSQLiteDatabase;
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {Person.class}, version = 1)
+public abstract class PersRoomDatabase extends RoomDatabase {
+
+    public abstract PersDao persDao();
+
+    private static volatile PersRoomDatabase INSTANCE;
+
+    static PersRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (PersRoomDatabase.class) {
+                if (INSTANCE == null) {
+
+                    // Create database here
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            PersRoomDatabase.class, "persons_database")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
